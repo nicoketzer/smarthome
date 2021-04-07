cca6550e8605f387805e4680dac908f062e0d21a
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2021-04-07 18:25:09 +0200

    Update install.php
    1.Umziehen der valid_json-Funktion
    2. Entfernen aller "Nicht-JSON"-Abläufen
    
    Was noch gemacht gehört:
    Automatische Überprüfung auch in JSON implementieren

--- c&c-server/install.php
+++ c&c-server/install.php
@@ -38,11 +38,6 @@
     include($dep_file);
     //Herunterladen des Repo´s
     do_pre_install();
-    
-    function valid_json($json) {
-        json_decode($json);
-        return (json_last_error() == JSON_ERROR_NONE);
-    }
     //JSON Anfragenimplementierung
     if(isset($_GET["json"])){
         //Überprüfen ob JSON String valides JSON ist
@@ -104,119 +99,6 @@
         exit;
     }
     //ENDE JSON IMPLEMEETIERUNG
-    
-    //Zur Verfügung stellen der CSS und JS Dateien
-    if(isset($_GET["include_file"]) && isset($_GET["file_name"])){
-        if($_GET['include_file'] == "css"){
-            //Setzen des Content-Type Headers
-            header("Content-Type: text/css");
-            if($_GET['file_name'] == "style.css"){
-                $url = "https://raw.githubusercontent.com/nicoketzer/smarthome/master/c%26c-server/install_files/style.css";
-                $process = curl_init($url);
-                curl_setopt($process, CURLOPT_HTTPHEADER, array ('content-type: text/plain',"Cache-Control: no-cache"));
-                curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-                curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-                //Damit immer die neuste Version von GitHub gezogen wird
-                curl_setopt($process, CURLOPT_FRESH_CONNECT, TRUE);
-                $response_body = curl_exec($process);
-                $http_code = curl_getinfo($process, CURLINFO_HTTP_CODE);
-                if($http_code >= 300) {
-                  die("Unexpected Response Code: ${http_code}: ${response_body}");
-                }
-                curl_close($process);
-                echo $response_body;
-                exit;    
-            }else{
-                die("Unknown File-Name");
-            }
-        }else if($_GET['include_file'] == "js"){
-            //Setzen des Content-Type Headers
-            header("Content-Type: text/javascript");
-            if($_GET['file_name'] == "main.js"){
-                $url = "https://raw.githubusercontent.com/nicoketzer/smarthome/master/c%26c-server/install_files/main.js";
-                $process = curl_init($url);
-                curl_setopt($process, CURLOPT_HTTPHEADER, array ('content-type: text/plain',"Cache-Control: no-cache"));
-                curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-                curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-                //Damit immer die neuste Version von GitHub gezogen wird
-                curl_setopt($process, CURLOPT_FRESH_CONNECT, TRUE);
-                $response_body = curl_exec($process);
-                $http_code = curl_getinfo($process, CURLINFO_HTTP_CODE);
-                if($http_code >= 300) {
-                  die("Unexpected Response Code: ${http_code}: ${response_body}");
-                }
-                curl_close($process);
-                //Aufbereiten des JS Codes und einfügen des neuen Server´s
-                $tmp = explode("[fill_in_server]",$response_body);
-                $server_ip = $_SERVER["HTTP_REFERER"];
-                $response_body = $tmp[0] . $server_ip . $tmp[1];
-                echo $response_body;
-                exit;
-            }else{
-                die("Unknown File-Name");
-            }
-        }else if($_GET['include_file'] == "html"){
-            header("Content-Type: text/html");
-            if($_GET['file_name'] == "fillout_form.html"){
-                $url = "https://raw.githubusercontent.com/nicoketzer/smarthome/master/c%26c-server/install_files/fillout_form.html";
-                $process = curl_init($url);
-                curl_setopt($process, CURLOPT_HTTPHEADER, array ('content-type: text/plain',"Cache-Control: no-cache"));
-                curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-                curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-                //Damit immer die neuste Version von GitHub gezogen wird
-                curl_setopt($process, CURLOPT_FRESH_CONNECT, TRUE);
-                $response_body = curl_exec($process);
-                $http_code = curl_getinfo($process, CURLINFO_HTTP_CODE);
-                if($http_code >= 300) {
-                  die("Unexpected Response Code: ${http_code}: ${response_body}");
-                }
-                curl_close($process);
-                //Bekommene HTML in JSON umwandeln
-                $tmp_array = array("content"=>array("main"=>$response_body, "title"=>"", "ref"=>"", "error" => "none"), "debug"=>array("response_code"=>$http_code, "get_url"=>$url, "request_file"=>$_GET['file_name']));
-                $json = json_encode($tmp_array);
-                echo $json;
-                exit;    
-            }else{
-                die("Unkown File-Name");
-            }    
-        }else{
-            //Kein Bekannter Include-Datei-Typ
-            die("Unexpected include_file");
-        }
-    }
-    
-    //Normale Funktionen
-    if(isset($_POST["start_install"]) && read_file("stage") == ""){
-        do_install();
-    }else{
-        $s = read_file("stage");
-        if($s == "1"){
-            //Benutzerangben müssen gemacht werden
-            if(isset($_POST["data_input"])){
-                //Daten kommen
-            }else{
-                //Ignorieren keine Daten kommen
-            }
-        }else if($s == "2"){
-            //Test der Mysqli Verbindung
-        }else if($s == "2.1"){
-            //Einrichten der Mysql-Umgebung
-            
-        }else if($s == "3"){
-            //Bekommen eines IP-Update-Tokens
-        }else if($s == "3.1"){
-            //Erstes IP-Update
-        }else if($s == "3.2"){
-            //Anfragen des "Self-Test"-URL´s für Stage 4
-        }else if($s == "4"){
-            //Erreichbarkeit prüfen(IP+Port+/install.php?c=test)
-            $self_test_url = read_file("self_test_url");
-            self_test($self_test_url);
-        }else if($s == "5"){
-            //Installation abschließen
-            finish_install();    
-        }
-    }
 ?>
 <html>
     <head>
